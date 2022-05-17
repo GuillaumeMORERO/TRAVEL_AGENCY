c5019aef94c606030f17cea1f62daf880dd594db
Guillaume <boolabitt@hotmail.com>
2022-05-17 17:28:15 +0200

    test ok

--- tests/Unit/VoyageTest.php
+++ tests/Unit/VoyageTest.php
@@ -19,35 +19,25 @@ class VoyageTest extends TestCase
      */
     public function testRoute()
     {
-        /*
-        / get home
-        */
         $getWelcome = $this->get('/');
         $getWelcome->assertStatus(200)
                 ->assertViewIs('vueVoyages');
-
-
     }
 
     public function testCreaVoyage()
     {
-        // $fakeUser = User::factory()->make();
-
-
-        // $admin = User::where('admin', '=', 1)->first();
-        // $userTodel = User::where('email', 'testCrea@testCrea.fr')->first();
-        // $testDeleteUser = $this->actingAs($admin)->delete('delUser/'.$userTodel->getid());
-        // $testDeleteUser->assertJson(['status' => 'success']);
-        //         $this->assertSoftDeleted($userTodel);
-
-
-
         $testRegisterVoyage = $this->post('/postVoyage', [
-            'departure_date' => '1960-01-01',
-            'arrival_date' => '1970-12-31',
+            'departure_date' => '1980-01-01 01:01:01',
+            'arrival_date' => '1980-12-12 01:01:01',
         ]);
-        // $this->assertDatabaseHas('voyages', ['departure_date' => '1960-01-01']);
+        $this->assertDatabaseHas('voyages', ['departure_date' => '1980-01-01 01:01:01']);
+    }
 
-        
+    public function testDeleteVoyage()
+    {
+        $testVoyage = Voyage::where('departure_date', '1980-01-01 01:01:01')->first();
+        $testDeleteVoyage = $this->get('/delVoyage/'.$testVoyage->id);
+        $testDeleteVoyage->assertStatus(302);
+        $this->assertDatabaseMissing('voyages', ['departure_date' => '1980-01-01 01:01:01']);
     }
 }
